751377838cf062f5d3c22f85e045108646801ad8
dhgoldberg <davidhgoldberg@gmail.com>
2014-11-07 19:54:22 -0500

    Better GAE Integration
    YoAll returns 201 instead of 200.  What's up with that?

--- yopy.py
+++ yopy.py
@@ -3,63 +3,93 @@
 A simple Yo! API for python.
 
 As of now, you can:
-	1.Get the number of subscribers
-	2.Send a Yo! to all subscribers
-	3.Send a Yo! to a specific user
+    1.Get the number of subscribers
+    2.Send a Yo! to all subscribers
+    3.Send a Yo! to a specific user
 
 YoPy requires a Yo! API access token. You can get one by registering at http://dev.justyo.co/
 
 Parth Dhar
 2014
+
+GAE integration by David H Goldberg 2014
 """
 from google.appengine.api import urlfetch
+import json
 
 class Yo:
-	def __init__(self, token):
-		self.token = token
+    def __init__(self, token):
+        self.token = token
 
-	def number(self):
-		"""
-		Function to GET the the number of subscribers of the API user account.
-		Returns number of subscribers as an integer.
-		If request is unsuccessful, raises an error.
-		"""
-		number_url = "https://api.justyo.co/subscribers_count/?api_token=" + self.token
-		number = urlfetch.fetch(number_url)
-		if number.status_code == requests.codes.ok:
-		    return number.json()["result"]
-		else:
-			number.raise_for_status()
+    def number(self):
+        """
+        Function to GET the the number of subscribers of the API user account.
 
-	def yo_all(self, *link):
-		"""
-		Function to send a Yo to all subscribers of the API user account.
-		If request is successful, returns true.
-		If request is unsuccessful, raises an error.
-		"""
-		yoall_data = {"api_token": self.token}
-		for kw in kwargs:
-			youser_data.update( { kw:kwargs[kw] } )
-		yoall_url = "https://api.justyo.co/yoall/"
-		yoall = urlfetch.fetch(url=yoall_url, payload=yoall_data, method=urlfetch.POST)
-		if yoall.status_code == requests.codes.created:
-			return True
-		else:
-			yoall.raise_for_status()
+        Returns:
+            int: number of subscribers if request is successful.
 
-	def yo_user(self, username, **kwargs):
-		"""
-		Function to send a Yo to a specific username.
-		If request is successful, returns true.
-		If request is unsuccessful, raises an error.
-		"""
-		username = username.upper()
-		youser_data = {"api_token": self.token, "username": username}
-		for kw in kwargs:
-			youser_data.update( { kw:kwargs[kw] } )
-		youser_url = "https://api.justyo.co/yo/"
-		youser = urlfetch.fetch(url=youser_url, payload=youser_data, method=urlfetch.POST)
-		if youser.status_code == requests.codes.ok:
-		    return True
-		else:
-			yoall.raise_for_status()
+        Raises:
+            Exception: if request is unsuccessful.
+        """
+        number_url = "https://api.justyo.co/subscribers_count/?api_token=" + self.token
+        number = urlfetch.fetch(number_url)
+        if number.status_code == 200:
+            inObj = json.loads(number.content)
+            return inObj['result']
+        else:
+            raise Exception(number.status_code)
+
+    def yo_all(self, link=''):
+        """
+        Function to send a Yo to all subscribers of the API user account.
+
+        Args:
+            link (str, optional): link to send with Yo. Default is neglected.
+
+        Returns:
+            bool: True if request is successful.
+
+        Raises:
+            Exception: if request is unsuccessful.
+        """
+        yoall_data = {"api_token": self.token, 'link':link}
+        yoall_url = "https://api.justyo.co/yoall/"
+        yoall = urlfetch.fetch(url=yoall_url,
+                               payload=json.dumps(yoall_data),
+                               method=urlfetch.POST,
+                               headers={'content-type': 'application/json'})
+        if yoall.status_code == 201: # not sure why 201 and not 200... 
+            return True
+        else:
+            raise Exception(yoall.status_code)
+
+    def yo_user(self, username, **kwargs):
+        """
+        Function to send a Yo to a specific username.
+        
+        Args:
+            username (str): the Yo usernname.
+            **kwargs: keyword should be either ''link'' or ''location''.
+                Location should be of the form ''lat,lon'' where lat and lon are decimal degrees.
+
+        Returns:
+            bool: True if request is successful.
+
+        Raises:
+            Exception: if request is unsuccessful.
+        """
+        username = username.upper()
+        youser_data = {"api_token": self.token, "username": username}
+        for kw in kwargs:
+            youser_data.update( { kw:kwargs[kw] } )
+        print json.dumps(youser_data)
+        youser_url = "https://api.justyo.co/yo/"
+        youser = urlfetch.fetch(url=youser_url, 
+                                payload=json.dumps(youser_data), 
+                                method=urlfetch.POST,
+                                headers={'content-type': 'application/json'})
+        print youser.status_code
+        if youser.status_code == 200:
+            return True
+        else:
+            raise Exception(youser.status_code)
